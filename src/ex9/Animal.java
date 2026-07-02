40585f6cb640c29f937a014d726793edfbc21ebd
Nicolas <adilsonq1976@gmail.com>
2026-07-02 11:08:31 -0300

    Exercicio 9

---
+++ src/ex9/Animal.java
@@ -0,0 +1,10 @@
+package ex9;
+
+public class Animal {
+    private String nome;
+    private int idade;
+
+    public void emitirSom(){
+        System.out.println("Som generico de animal");
+    }
+}
