0d2d412883cd583f1993aed20de8517a8f0f4071
Nicolas <adilsonq1976@gmail.com>
2026-07-02 10:36:55 -0300

    Exercicio 6

---
+++ src/ex6/Main.java
@@ -0,0 +1,10 @@
+package ex6;
+
+public class Main {
+    public static void main(String[] args) {
+        Retangulo retangulo1 = new Retangulo(2,4);
+
+        System.out.println("Area: " + retangulo1.calcularArea());
+        System.out.println("Perimetro: " + retangulo1.calcularPerimetro());
+    }
+}
