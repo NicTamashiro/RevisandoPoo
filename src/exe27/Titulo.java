e8ad2f2a8bed95d06ddc1491b35d3c44781de1cc
Nicolas <adilsonq1976@gmail.com>
2026-07-03 16:48:29 -0300

    Exercicio 27

---
+++ src/exe27/Titulo.java
@@ -0,0 +1,6 @@
+package exe27;
+
+import com.google.gson.annotations.SerializedName;
+
+public record Titulo(@SerializedName("Title") String titulo, @SerializedName("Year") String anoLancamento, @SerializedName("imdbRating") String avaliacao) {
+}
