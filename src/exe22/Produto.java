579f84cafee67da5bac1e7209e8146dab4d91e84
Nicolas <adilsonq1976@gmail.com>
2026-07-03 11:22:06 -0300

    Exercicio 22

---
+++ src/exe22/Produto.java
@@ -0,0 +1,29 @@
+package exe22;
+
+public class Produto implements Comparable<Produto>{
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public int compareTo(Produto outroProduto) {
+        return Double.compare(this.preco, outroProduto.preco);
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + nome + " (preco: R$ " + preco + ")";
+    }
+}
