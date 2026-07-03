e79ccd5364e1f96e4f85324830e2b73c1b48aaad
Nicolas <adilsonq1976@gmail.com>
2026-07-03 10:28:30 -0300

    exercicio 15

---
+++ src/exe15/Main.java
@@ -0,0 +1,19 @@
+package exe15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Filme> filmes = new ArrayList<>();
+
+        filmes.add(new Filme("Matrix", 8.7));
+        filmes.add(new Filme("Coraline", 8.5));
+        filmes.add(new Filme("Diario de uma paixao", 8.8));
+        filmes.add(new Filme("Gran Turismo", 8.6));
+
+        for (Filme filme : filmes){
+            System.out.println(filme);
+        }
+    }
+}
