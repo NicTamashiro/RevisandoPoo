08ecc23e3c48418dc1798a8d42a8306acf9ccec1
Nicolas <adilsonq1976@gmail.com>
2026-07-03 16:52:45 -0300

    Exercicio 28

---
+++ src/exe28/Main.java
@@ -0,0 +1,28 @@
+package exe28;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.omdbapi.com/?t=Seven&apikey=1da600ca")).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            Gson gson = new Gson();
+            Titulo titulo = gson.fromJson(response.body(), Titulo.class);
+
+            System.out.println(titulo);
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/exe28/Titulo.java
@@ -0,0 +1,6 @@
+package exe28;
+
+import com.google.gson.annotations.SerializedName;
+
+public record Titulo(@SerializedName("Title") String titulo, @SerializedName("Year") String anoLancamento, @SerializedName("imdbRating") String avaliacao) {
+}
